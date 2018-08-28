7626651157734491bd42978714bc426f9bb7bd71
n.igarashi <nobu.igaigasan@gmail.com>
2018-08-28 10:49:03 +0900

    コミット

--- test-github/ViewController.swift
+++ test-github/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //Sample//
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
